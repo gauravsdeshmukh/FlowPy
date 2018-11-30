d8b441dd2ee73985f002eabe7e109c5be541b8aa
Gaurav Deshmukh <39861664+gauravsdeshmukh@users.noreply.github.com>
2018-11-30 19:10:10 +0530

    Add files via upload

--- ReadAndPlot.py
+++ ReadAndPlot.py
@@ -14,12 +14,12 @@ import matplotlib.animation as animation
 os.chdir("Result")
 cwdir=os.getcwd()
 
-final_iter=1488
-inter=2
-rowpts=100
-colpts=100
-length=4
-breadth=4
+final_iter=3995
+inter=10
+rowpts=200
+colpts=200
+length=1
+breadth=1
 
 number_of_frames=int(final_iter/inter)+1
 
@@ -29,19 +29,19 @@ y=sci.linspace(0,breadth,rowpts)
 fig=plt.figure(figsize=(16,8))
 ax=plt.axes(xlim=(0,length),ylim=(0,breadth))
 
-for i in range(0,final_iter+inter,inter):
-    arr=sci.loadtxt(f"PUV{i}.txt",delimiter="\t")
-    rows,cols=arr.shape
-    p_p=sci.zeros((rowpts,colpts))
-    u_p=sci.zeros((rowpts,colpts))
-    v_p=sci.zeros((rowpts,colpts))
-    p_arr=arr[:,0]
-    u_arr=arr[:,1]
-    v_arr=arr[:,2]
-    
-    p_p=p_arr.reshape((rowpts,colpts))
-    u_p=u_arr.reshape((rowpts,colpts))
-    v_p=v_arr.reshape((rowpts,colpts))
+#for i in range(0,final_iter+inter,inter):
+#    arr=sci.loadtxt(f"PUV{i}.txt",delimiter="\t")
+#    rows,cols=arr.shape
+#    p_p=sci.zeros((rowpts,colpts))
+#    u_p=sci.zeros((rowpts,colpts))
+#    v_p=sci.zeros((rowpts,colpts))
+#    p_arr=arr[:,0]
+#    u_arr=arr[:,1]
+#    v_arr=arr[:,2]
+#    
+#    p_p=p_arr.reshape((rowpts,colpts))
+#    u_p=u_arr.reshape((rowpts,colpts))
+#    v_p=v_arr.reshape((rowpts,colpts))
 
     
 def animate(i):
@@ -60,9 +60,13 @@ def animate(i):
     u_p=u_arr.reshape((rowpts,colpts))
     v_p=v_arr.reshape((rowpts,colpts))
     
+    ax.clear()
+    ax.set_xlim([0,length])
+    ax.set_ylim([0,breadth])
+    ax.set_title(f"Frame No: {i}")
     cont=ax.contourf(X,Y,p_p)
-    quiv=ax.quiver(X,Y,u_p,v_p)
-    return cont,quiv
+    stream=ax.streamplot(X,Y,u_p,v_p,color="k")
+    return cont,stream
 
-anim=animation.FuncAnimation(fig,animate,frames=number_of_frames,interval=100)
-#anim.save("PlugFlow.mp4")
+anim=animation.FuncAnimation(fig,animate,frames=number_of_frames,interval=50,blit=False)
+anim.save("LidCavity_Benchmark.mp4")
